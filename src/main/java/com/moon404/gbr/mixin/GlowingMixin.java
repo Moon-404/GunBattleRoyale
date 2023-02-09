787a40cd2f41d6de6ba15149f442b6b45f20794c
ZhouZiHao-Moon <zhouzihao2008@126.com>
2023-02-09 14:22:51 +0800

    1.3.3.24
    使用MIXIN制作了这个效果：
    
    - 战斗中的玩家会高亮显示队友位置

--- src/main/java/com/moon404/gbr/mixin/GlowingMixin.java
+++ src/main/java/com/moon404/gbr/mixin/GlowingMixin.java
@@ -6,9 +6,11 @@ import org.spongepowered.asm.mixin.Shadow;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
+import net.minecraft.client.multiplayer.PlayerInfo;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.GameType;
 
 @Mixin(Minecraft.class)
 public class GlowingMixin
@@ -18,6 +20,13 @@ public class GlowingMixin
     @Shadow
     public Options options;
 
+    public boolean isAdventure(LocalPlayer localPlayer)
+    {
+        if (localPlayer == null) return false;
+        PlayerInfo playerinfo = Minecraft.getInstance().getConnection().getPlayerInfo(localPlayer.getGameProfile().getId());
+        return playerinfo != null && playerinfo.getGameMode() == GameType.ADVENTURE;
+    }
+
     @Overwrite
     public boolean shouldEntityAppearGlowing(Entity p_91315_)
     {
@@ -25,8 +34,8 @@ public class GlowingMixin
         if (this.player != null && this.player.isSpectator() && this.options.keySpectatorOutlines.isDown() && p_91315_.getType() == EntityType.PLAYER) return true;
         // 有发光效果必定发光
         if (p_91315_.isCurrentlyGlowing()) return true;
-        // 没有发光效果，相同队伍也会发光
-        if (this.player != null && this.player.getTeam() == p_91315_.getTeam()) return true;
+        // 自己[在战斗中]，显示所有[同队][玩家]
+        if (isAdventure(this.player) && p_91315_.getType() == EntityType.PLAYER && this.player.getTeam() == p_91315_.getTeam()) return true;
         // 否则不发光
         return false;
     }
