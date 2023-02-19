ef377e8779535d9dac3dc6559ecef2d85713a3f1
ZhouZiHao-Moon <zhouzihao2008@126.com>
2023-02-19 18:02:43 +0800

    fix bug

--- src/main/java/com/moon404/gbr/EventHandler.java
+++ src/main/java/com/moon404/gbr/EventHandler.java
@@ -11,16 +11,28 @@ import net.minecraftforge.fml.LogicalSide;
 
 public class EventHandler
 {
+    public static Map<Player, Double> lastX = new HashMap<Player, Double>();
+    public static Map<Player, Double> lastY = new HashMap<Player, Double>();
+    public static Map<Player, Double> lastZ = new HashMap<Player, Double>();
     public static Map<Player, Double> speedMap = new HashMap<Player, Double>();
     @SubscribeEvent
     public static void onPlayerTick(PlayerTickEvent event)
     {
         if (event.phase != Phase.END) return;
-        if (event.side != LogicalSide.CLIENT) return;
+        if (event.side != LogicalSide.SERVER) return;
         Player player = event.player;
-        double dx = player.getX() - player.xOld;
-        double dy = player.getY() - player.yOld;
-        double dz = player.getZ() - player.zOld;
+        double dx = 0.0F;
+        double dy = 0.0F;
+        double dz = 0.0F;
+        double x = player.getX();
+        double y = player.getY();
+        double z = player.getZ();
+        if (lastX.containsKey(player)) dx = x - lastX.get(player);
+        if (lastY.containsKey(player)) dy = y - lastY.get(player);
+        if (lastZ.containsKey(player)) dz = z - lastZ.get(player);
+        lastX.put(player, x);
+        lastY.put(player, y);
+        lastZ.put(player, z);
         double d = Math.sqrt(dx * dx + dy * dy + dz * dz);
         speedMap.put(player, d);
     }
