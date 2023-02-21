4b4c680cfe825c115c2f8e3432581001eb9a75ef
ZhouZiHao-Moon <zhouzihao2008@126.com>
2023-02-21 09:02:13 +0800

    1.3.3.28a
    - 半自动枪械的后座时间延长至10tick
    - 全自动枪械的枪口上抬时间与射击间隔一致

--- src/main/java/com/moon404/gbr/mixin/RecoilMixin.java
+++ src/main/java/com/moon404/gbr/mixin/RecoilMixin.java
@@ -11,9 +11,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.mrcrayfish.guns.Config;
 import com.mrcrayfish.guns.client.handler.RecoilHandler;
+import com.mrcrayfish.guns.common.Gun;
+import com.mrcrayfish.guns.common.Gun.General;
 import com.mrcrayfish.guns.event.GunFireEvent;
+import com.mrcrayfish.guns.item.GunItem;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -21,7 +25,8 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class RecoilMixin
 {
     private Random random = new Random();
-    private static float offsetAngle;
+    private float offsetAngle;
+    private int rate;
 
     @Shadow
     private float cameraRecoil;
@@ -31,7 +36,13 @@ public class RecoilMixin
     @Inject(method = "onGunFire", at = @At("TAIL"), remap = false)
     private void onGunFireTail(GunFireEvent.Post event, CallbackInfo ci)
     {
-        offsetAngle = this.random.nextFloat(-0.5F, 0.5F); // 大约30度
+        this.offsetAngle = this.random.nextFloat(-0.5F, 0.5F); // 大约30度
+        ItemStack heldItem = event.getStack();
+        GunItem gunItem = (GunItem)heldItem.getItem();
+        Gun modifiedGun = gunItem.getModifiedGun(heldItem);
+        General general = modifiedGun.getGeneral();
+        this.rate = general.getRate();
+        if (!general.isAuto()) this.rate = 2;
     }
 
     @Overwrite(remap = false)
@@ -45,14 +56,14 @@ public class RecoilMixin
 
         if (!Config.SERVER.enableCameraRecoil.get()) return;
 
-        float recoilAmount = this.cameraRecoil * mc.getDeltaFrameTime() * 0.15F;
+        float recoilAmount = this.cameraRecoil * mc.getDeltaFrameTime() * 0.2F / this.rate;
         float startProgress = this.progressCameraRecoil / this.cameraRecoil;
         float endProgress = (this.progressCameraRecoil + recoilAmount) / this.cameraRecoil;
 
         float pitch = mc.player.getXRot();
         float yaw = mc.player.getYRot();
-        float xrecoil = this.cameraRecoil * (float)Math.cos(offsetAngle);
-        float yrecoil = this.cameraRecoil * (float)Math.sin(offsetAngle);
+        float xrecoil = this.cameraRecoil * (float)Math.cos(this.offsetAngle);
+        float yrecoil = this.cameraRecoil * (float)Math.sin(this.offsetAngle);
         if (startProgress < 0.2F)
         {
             mc.player.setXRot(pitch - ((endProgress - startProgress) / 0.2F) * xrecoil);
