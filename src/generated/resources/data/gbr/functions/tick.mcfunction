f916311214f69dabfaadb1ae9844946b2f609b4c
ZhouZiHao-Moon <zhouzihao2008@126.com>
2023-02-24 20:10:15 +0800

    修改段位粒子渲染逻辑
    现在只在客户端渲染粒子，不再使用服务器发送粒子信息，理论上可以降低服务器的带宽需求。

--- src/generated/resources/data/gbr/functions/tick.mcfunction
+++ src/generated/resources/data/gbr/functions/tick.mcfunction
@@ -3,9 +3,6 @@ execute as @a store result score @s height run data get entity @s Pos[1]
 execute as @a store result score @s absorption run data get entity @s AbsorptionAmount
 execute as @a store result score @s maxhealth run attribute @s generic.max_health get
 
-# 尾迹
-execute as @a[gamemode=adventure, scores={height=40..200}] at @s run function gbr:tick/rank_particle
-
 # 游戏内&游戏外
 execute if score game_start global matches 0 run function gbr:tick/out_game
 execute if score game_start global matches 1 run function gbr:tick/in_game
---
+++ src/main/java/com/moon404/gbr/RankParticles.java
@@ -0,0 +1,54 @@
+package com.moon404.gbr;
+
+import com.mojang.math.Vector3f;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.particles.DustParticleOptions;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ElytraItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.scores.Objective;
+import net.minecraft.world.scores.Scoreboard;
+import net.minecraftforge.event.TickEvent.ClientTickEvent;
+import net.minecraftforge.event.TickEvent.Phase;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class RankParticles
+{
+    @SubscribeEvent
+    public static void onClientTick(ClientTickEvent event)
+    {
+        if (event.phase != Phase.END) return;
+        Minecraft mc = Minecraft.getInstance();
+        ClientLevel clientLevel = mc.level;
+        if (clientLevel == null) return;
+        Scoreboard scoreboard = clientLevel.getScoreboard();
+        Iterable<Entity> entities = clientLevel.entitiesForRendering();
+        for (Entity entity : entities)
+        {
+            if (entity instanceof Player)
+            {
+                Objective objective = scoreboard.getObjective("global");
+                int game_start = scoreboard.getOrCreatePlayerScore("game_start", objective).getScore();
+                Item item = ((Player)entity).getItemBySlot(EquipmentSlot.CHEST).getItem();
+                if (game_start == 0 || item.getClass() == ElytraItem.class)
+                {
+                    objective = scoreboard.getObjective("rank");
+                    int score = scoreboard.getOrCreatePlayerScore(entity.getScoreboardName(), objective).getScore();
+                    Vector3f color;
+                    if (score >= 150) color = new Vector3f(1.00F, 0.25F, 0.21F);
+                    else if (score >= 100) color = new Vector3f(0.69F, 0.05F, 0.79F);
+                    else if (score >= 60) color = new Vector3f(0.00F, 0.45F, 0.85F);
+                    else if (score >= 30) color = new Vector3f(0.50F, 0.86F, 1.00F);
+                    else if (score >= 10) color = new Vector3f(1.00F, 0.86F, 0.00F);
+                    else color = new Vector3f(0.75F, 0.75F, 0.75F);
+                    DustParticleOptions options = new DustParticleOptions(color, 2.0F);
+                    clientLevel.addParticle(options, true, entity.getX(), entity.getY() + 0.2F, entity.getZ(), 0, 0, 0);
+                }
+            }
+        }
+    }
+}
