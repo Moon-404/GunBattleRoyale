c99f087c6f51e70519a29416f118d89ed08bf854
ZhouZiHao-Moon <zhouzihao2008@126.com>
2023-03-06 09:01:04 +0800

    更新伤害公式
    由 (life-射出时间+1)/life 变为 (life-射出时间)/(life+1)，将所有枪械的 life 降低 1，解决了部分枪械（尤其是冲锋枪和霰弹枪）在近距离伤害反而更高的问题和枪械射程超过预设值的问题。

---
+++ src/main/java/com/moon404/gbr/mixin/DamageMixin.java
@@ -0,0 +1,42 @@
+package com.moon404.gbr.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import com.mrcrayfish.guns.common.Gun;
+import com.mrcrayfish.guns.entity.ProjectileEntity;
+import com.mrcrayfish.guns.util.GunEnchantmentHelper;
+import com.mrcrayfish.guns.util.GunModifierHelper;
+
+import net.minecraft.world.item.ItemStack;
+
+@Mixin(ProjectileEntity.class)
+public class DamageMixin extends EntityMixin
+{
+    @Shadow
+    protected Gun.Projectile projectile;
+    @Shadow
+    protected float additionalDamage;
+    @Shadow
+    private ItemStack weapon;
+    @Shadow
+    protected Gun modifiedGun;
+    @Shadow
+    protected Gun.General general;
+
+    @Overwrite(remap = false)
+    public float getDamage()
+    {
+        float initialDamage = (this.projectile.getDamage() + this.additionalDamage);
+        if (this.projectile.isDamageReduceOverLife())
+        {
+            float modifier = (float)(this.projectile.getLife() - this.tickCount) / (float)(this.projectile.getLife() + 1);
+            initialDamage *= modifier;
+        }
+        float damage = initialDamage / this.general.getProjectileAmount();
+        damage = GunModifierHelper.getModifiedDamage(this.weapon, this.modifiedGun, damage);
+        damage = GunEnchantmentHelper.getAcceleratorDamage(this.weapon, damage);
+        return Math.max(0F, damage);
+    }
+}
---
+++ src/main/java/com/moon404/gbr/mixin/EntityMixin.java
@@ -0,0 +1,13 @@
+package com.moon404.gbr.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import net.minecraft.world.entity.Entity;
+
+@Mixin(Entity.class)
+public class EntityMixin
+{
+    @Shadow
+    public int tickCount;
+}
