d2896ea9d2f73a8473f51e70e1eee99456182e53
ZhouZiHao-Moon <zhouzihao2008@126.com>
2023-03-14 11:02:25 +0800

    将遗体中的物品排序

--- src/main/java/com/moon404/gbr/handler/CorpseDeathHandler.java
+++ src/main/java/com/moon404/gbr/handler/CorpseDeathHandler.java
@@ -1,11 +1,15 @@
 package com.moon404.gbr.handler;
 
+import java.util.Collections;
+import java.util.Comparator;
+
 import com.mrcrayfish.guns.common.Gun;
 import com.mrcrayfish.guns.item.GunItem;
 import com.mrcrayfish.guns.item.attachment.IAttachment.Type;
 
 import de.maxhenkel.corpse.corelib.death.Death;
 import de.maxhenkel.corpse.corelib.death.PlayerDeathEvent;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -31,6 +35,17 @@ public class CorpseDeathHandler
                 }
                 itemStack.getOrCreateTag().remove("Attachments");
             }
+        }
+        Collections.sort(death.getMainInventory(), new ItemStackComparator());
+        Collections.sort(death.getAdditionalItems(), new ItemStackComparator());
+    }
+
+    private static class ItemStackComparator implements Comparator<ItemStack>
+    {
+        @Override
+        public int compare(ItemStack i1, ItemStack i2)
+        {
+            return Item.getId(i2.getItem()) - Item.getId(i1.getItem());
         }
     }
 }
